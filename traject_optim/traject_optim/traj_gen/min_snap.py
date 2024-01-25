c7b5516691764af6251ebc4321925acf2dcb9782
Shaswat2001 <sis_shaswat@outlook.com>
2024-01-25 10:34:18 -0500

    fix: remove bugs from constraint function

--- traject_optim/traject_optim/traj_gen/min_snap.py
+++ traject_optim/traject_optim/traj_gen/min_snap.py
@@ -6,93 +6,88 @@ path = [np.array([40, 18,  3]), np.array([30.41, 16.08,  2.8 ]), np.array([30.47
 
 n_points = len(path)
 n_splines = n_points - 1
+velocity = 1
+dt = 0.01
+
+time = np.zeros((n_points))
+init = 0
+for i in range(1,len(path)):
+
+    dist = np.linalg.norm(path[i] - path[i-1])
+    time[i] = time[i-1] + dist/velocity
+
+print(time)
 position = []
 x = []
 y = []
 z = []
-
 vel = []
-velocity = 1
-dt = 0.01
 
-for i in range(n_splines):
 
-    p0 = path[i]
-    p1 = path[i+1]
-    dist = np.linalg.norm(p1 - p0)
-    T = np.round(dist/velocity,2)
-    T7 = T**7
-    T6 = T**6
-    T5 = T**5
-    T4 = T**4
-    T3 = T**3
-    T2 = T**2
+A = np.zeros((4* n_splines,4* n_splines,3))
+X = np.zeros((4* n_splines,3))
+B = np.zeros((4* n_splines,3))
 
-    A = np.array([[0,0,0,0,0,0,0,1],
-                  [T7,T6,T5,T4,T3,T2,T,1],
-                  [0,0,0,0,0,0,1,0],
-                  [7*T6,6*T5,5*T4,4*T3,3*T2,2*T,1,0],
-                  [0,0,0,0,0,2,0,0],
-                  [42*T5,30*T4,20*T3,12*T2,6*T,2,0,0],
-                  [0,0,0,0,6,0,0,0],
-                  [210*T4,120*T3,60*T2,24*T,6,0,0,0]])
+for i in range(3):
 
-    b = np.array([[p0[0],p0[1],p0[2]],
-                  [p1[0],p1[1],p1[2]],
-                  [0,0,0],
-                  [0,0,0],
-                  [0,0,0],
-                  [0,0,0],
-                  [0,0,0],
-                  [0,0,0]])
-    
-    # if i == 0:
-    #     b = np.array([[p0[0],p0[1],p0[2]],
-    #               [p1[0],p1[1],p1[2]],
-    #               [0,0,0],
-    #               [0.58,0.58,0.58],
-    #               [0,0,0],
-    #               [0,0,0],
-    #               [0,0,0],
-    #               [0,0,0]])
+    A[:,:,i] = np.identity(4* n_splines)*(2.23e-16)
+    idx = 0
+    for k in range(0,n_splines-1):
+        A[idx,4*(k):4*(k+1),i] = np.array([time[k+1]**3,time[k+1]**2,time[k+1],1])
+        B[idx,i] = path[k+1][i]
+        idx += 1
+        A[idx,4*(k+1):4*(k+2),i] = np.array([time[k+1]**3,time[k+1]**2,time[k+1],1])
+        B[idx,i] = path[k+1][i]
+        idx += 1
 
-    # if i == n_splines - 1:
-    #     b = np.array([[p0[0],p0[1],p0[2]],
-    #               [p1[0],p1[1],p1[2]],
-    #               [0.58,0.58,0.58],
-    #               [0,0,0],
-    #               [0,0,0],
-    #               [0,0,0],
-    #               [0,0,0],
-    #               [0,0,0]])
-    
-    coeff,_,_,_ = np.linalg.lstsq(A,b,rcond=None)
+    for k in range(0,n_splines-1):
 
-    for i in np.arange(0,T,dt):
+        A[idx,4*(k):4*(k+1),i] = np.array([3*time[k+1]**2,2*time[k+1],1,0])
+        A[idx,4*(k+1):4*(k+2),i] = -np.array([3*time[k+1]**2,2*time[k+1],1,0])
+        B[idx,i] = 0
+        idx += 1
 
-        T7 = i**7
-        T6 = i**6
-        T5 = i**5
-        T4 = i**4
-        T3 = i**3
-        T2 = i**2
+    for k in range(0,n_splines-1):
 
-        A = np.array([[0,0,0,0,0,0,0,1],
-                    [T7,T6,T5,T4,T3,T2,i,1],
-                    [0,0,0,0,0,0,1,0],
-                    [7*T6,6*T5,5*T4,4*T3,3*T2,2*i,1,0],
-                    [0,0,0,0,0,2,0,0],
-                    [42*T5,30*T4,20*T3,12*T2,6*i,2,0,0],
-                    [0,0,0,0,6,0,0,0],
-                    [210*T4,120*T3,60*T2,24*i,6,0,0,0]])
+        A[idx,4*(k):4*(k+1),i] = np.array([6*time[k+1],2,0,0])
+        A[idx,4*(k+1):4*(k+2),i] = -np.array([6*time[k+1],2,0,0])
+        B[idx,i] = 0
+        idx += 1
+
+    k = 0
+    A[idx,4*(k):4*(k+1),i] = np.array([time[k]**3,time[k]**2,time[k],1])
+    B[idx,i] = path[k][i]
+    idx += 1
+    A[idx,4*(k):4*(k+1),i] = np.array([3*time[k]**2,2*time[k],1,0])
+    B[idx,i] = 0
+    idx += 1
+    k = n_splines - 1
+    A[idx,4*(k):4*(k+1),i] = np.array([time[k+1]**3,time[k+1]**2,time[k+1],1])
+    B[idx,i] = path[k+1][i]
+    idx += 1
+    A[idx,4*(k):4*(k+1),i] = np.array([3*time[k+1]**2,2*time[k+1],1,0])
+    B[idx,i] = 0
+    idx += 1
+    A[:,:,i] += np.identity(4* n_splines)*(2.23e-16)
+    X[:,i] = np.linalg.solve(A[:,:,i],B[:,i])
+
+result_t = 0
+for i in range(time.shape[0]-1):
+
+    t0 = time[i]
+
+    for T in np.arange(result_t,result_t+t0,dt):
         
-        sol = A @ coeff
-        position.append(sol[1,:3])
-        x.append(sol[1,0])
-        y.append(sol[1,1])
-        z.append(sol[1,2])
-        vel.append(sol[3,:3])
+        pos = np.array([T**3,T**2,T,1]).reshape(1,-1) @ X[4*i:4*(i+1),:]
+        position.append(pos)
+        x.append(pos[0,0])
+        y.append(pos[0,1])
+        z.append(pos[0,2])
+    
+    result_t += t0
 
+print(position[0])
+print(position[-1])
 fig = plt.figure()
 ax = plt.axes(projection='3d')
 ax.plot3D(x, y, z, 'gray')

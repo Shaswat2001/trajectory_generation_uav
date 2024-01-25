4c3402a910dadc293853dea544d22127457a848f
Shaswat2001 <sis_shaswat@outlook.com>
2024-01-25 12:20:57 -0500

    fix: remove bug from min snap trajectory

--- traject_optim/traject_optim/traj_gen/min_snap.py
+++ traject_optim/traject_optim/traj_gen/min_snap.py
@@ -24,51 +24,91 @@ z = []
 vel = []
 
 
-A = np.zeros((4* n_splines,4* n_splines,3))
-X = np.zeros((4* n_splines,3))
-B = np.zeros((4* n_splines,3))
+A = np.zeros((8* n_splines,8* n_splines,3))
+X = np.zeros((8* n_splines,3))
+B = np.zeros((8* n_splines,3))
 
 for i in range(3):
 
-    A[:,:,i] = np.identity(4* n_splines)*(1e-10)
+    A[:,:,i] = np.identity(8* n_splines)*(1e-10)
     idx = 0
     for k in range(0,n_splines-1):
-        A[idx,4*(k):4*(k+1),i] = np.array([time[k+1]**3,time[k+1]**2,time[k+1],1])
+        A[idx,8*(k):8*(k+1),i] = np.array([time[k+1]**7,time[k+1]**6,time[k+1]**5,time[k+1]**4,time[k+1]**3,time[k+1]**2,time[k+1],1])
         B[idx,i] = path[k+1][i]
         idx += 1
-        A[idx,4*(k+1):4*(k+2),i] = np.array([time[k+1]**3,time[k+1]**2,time[k+1],1])
+        A[idx,8*(k+1):8*(k+2),i] = np.array([time[k+1]**7,time[k+1]**6,time[k+1]**5,time[k+1]**4,time[k+1]**3,time[k+1]**2,time[k+1],1])
         B[idx,i] = path[k+1][i]
         idx += 1
 
     for k in range(0,n_splines-1):
 
-        A[idx,4*(k):4*(k+1),i] = np.array([3*time[k+1]**2,2*time[k+1],1,0])
-        A[idx,4*(k+1):4*(k+2),i] = -np.array([3*time[k+1]**2,2*time[k+1],1,0])
+        A[idx,8*(k):8*(k+1),i] = np.array([7*time[k+1]**6,6*time[k+1]**5,5*time[k+1]**4,4*time[k+1]**3,3*time[k+1]**2,2*time[k+1],1,0])
+        A[idx,8*(k+1):8*(k+2),i] = -np.array([7*time[k+1]**6,6*time[k+1]**5,5*time[k+1]**4,4*time[k+1]**3,3*time[k+1]**2,2*time[k+1],1,0])
         B[idx,i] = 0
         idx += 1
 
     for k in range(0,n_splines-1):
 
-        A[idx,4*(k):4*(k+1),i] = np.array([6*time[k+1],2,0,0])
-        A[idx,4*(k+1):4*(k+2),i] = -np.array([6*time[k+1],2,0,0])
+        A[idx,8*(k):8*(k+1),i] = np.array([42*time[k+1]**5,30*time[k+1]**4,20*time[k+1]**3,12*time[k+1]**2,6*time[k+1],2,0,0])
+        A[idx,8*(k+1):8*(k+2),i] = -np.array([42*time[k+1]**5,30*time[k+1]**4,20*time[k+1]**3,12*time[k+1]**2,6*time[k+1],2,0,0])
+        B[idx,i] = 0
+        idx += 1
+    
+    for k in range(0,n_splines-1):
+
+        A[idx,8*(k):8*(k+1),i] = np.array([210*time[k+1]**4,120*time[k+1]**3,60*time[k+1]**2,24*time[k+1],6,0,0,0])
+        A[idx,8*(k+1):8*(k+2),i] = -np.array([210*time[k+1]**4,120*time[k+1]**3,60*time[k+1]**2,24*time[k+1],6,0,0,0])
+        B[idx,i] = 0
+        idx += 1
+    
+    for k in range(0,n_splines-1):
+
+        A[idx,8*(k):8*(k+1),i] = np.array([840*time[k+1]**3,360*time[k+1]**2,120*time[k+1],24,0,0,0,0])
+        A[idx,8*(k+1):8*(k+2),i] = -np.array([840*time[k+1]**3,360*time[k+1]**2,120*time[k+1],24,0,0,0,0])
+        B[idx,i] = 0
+        idx += 1
+    
+    for k in range(0,n_splines-1):
+
+        A[idx,8*(k):8*(k+1),i] = np.array([2520*time[k+1]**2,720*time[k+1],120,0,0,0,0,0])
+        A[idx,8*(k+1):8*(k+2),i] = -np.array([2520*time[k+1]**2,720*time[k+1],120,0,0,0,0,0])
+        B[idx,i] = 0
+        idx += 1
+    
+    for k in range(0,n_splines-1):
+
+        A[idx,8*(k):8*(k+1),i] = np.array([5040*time[k+1],720,0,0,0,0,0,0])
+        A[idx,8*(k+1):8*(k+2),i] = -np.array([5040*time[k+1],720,0,0,0,0,0,0])
         B[idx,i] = 0
         idx += 1
 
     k = 0
-    A[idx,4*(k):4*(k+1),i] = np.array([time[k]**3,time[k]**2,time[k],1])
+    A[idx,8*(k):8*(k+1),i] = np.array([time[k]**7,time[k]**6,time[k]**5,time[k]**4,time[k]**3,time[k]**2,time[k],1])
     B[idx,i] = path[k][i]
     idx += 1
-    A[idx,4*(k):4*(k+1),i] = np.array([3*time[k]**2,2*time[k],1,0])
+    A[idx,8*(k):8*(k+1),i] = np.array([7*time[k]**6,6*time[k]**5,5*time[k]**4,4*time[k]**3,3*time[k]**2,2*time[k],1,0])
+    B[idx,i] = 0
+    idx += 1
+    A[idx,8*(k):8*(k+1),i] = np.array([42*time[k]**5,30*time[k]**4,20*time[k]**3,12*time[k]**2,6*time[k],2,0,0])
+    B[idx,i] = 0
+    idx += 1
+    A[idx,8*(k):8*(k+1),i] = np.array([210*time[k]**4,120*time[k]**3,60*time[k]**2,24*time[k],6,0,0,0])
     B[idx,i] = 0
     idx += 1
     k = n_splines - 1
-    A[idx,4*(k):4*(k+1),i] = np.array([time[k+1]**3,time[k+1]**2,time[k+1],1])
+    A[idx,8*(k):8*(k+1),i] = np.array([time[k+1]**7,time[k+1]**6,time[k+1]**5,time[k+1]**4,time[k+1]**3,time[k+1]**2,time[k+1],1])
     B[idx,i] = path[k+1][i]
     idx += 1
-    A[idx,4*(k):4*(k+1),i] = np.array([3*time[k+1]**2,2*time[k+1],1,0])
+    A[idx,8*(k):8*(k+1),i] = np.array([7*time[k+1]**6,6*time[k+1]**5,5*time[k+1]**4,4*time[k+1]**3,3*time[k+1]**2,2*time[k+1],1,0])
     B[idx,i] = 0
     idx += 1
-    A[:,:,i] += np.identity(4* n_splines)*(2.23e-16)
+    A[idx,8*(k):8*(k+1),i] = np.array([42*time[k+1]**5,30*time[k+1]**4,20*time[k+1]**3,12*time[k+1]**2,6*time[k+1],2,0,0])
+    B[idx,i] = 0
+    idx += 1
+    A[idx,8*(k):8*(k+1),i] = np.array([210*time[k+1]**4,120*time[k+1]**3,60*time[k+1]**2,24*time[k+1]**1,6,0,0,0])
+    B[idx,i] = 0
+    idx += 1
+    A[:,:,i] += np.identity(8* n_splines)*(2.23e-16)
     X[:,i] = np.linalg.lstsq(A[:,:,i],B[:,i],rcond=None)[0]
 
 # result_t = 0
@@ -76,7 +116,7 @@ for i in range(time.shape[0]-1):
 
     for T in np.arange(time[i],time[i+1],dt):
         
-        pos = np.array([T**3,T**2,T,1]).reshape(1,-1) @ X[4*i:4*(i+1),:]
+        pos = np.array([T**7,T**6,T**5,T**4,T**3,T**2,T,1]).reshape(1,-1) @ X[8*i:8*(i+1),:]
         position.append(pos)
         x.append(pos[0,0])
         y.append(pos[0,1])
